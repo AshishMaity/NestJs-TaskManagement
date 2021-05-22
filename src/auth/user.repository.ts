0088049702431a4dd7e874938ed96b40b19105d1
Ashish Maity <maity.ashish19@gmail.com>
2021-05-22 12:19:54 +0530

    Autthentication and Authorization wrap-up

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,43 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { EntityRepository, Repository } from "typeorm";
+import * as bcrypt from 'bcrypt';
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+  
+  async signUp(authCredentials:AuthCredentialsDto): Promise<void>{
+
+    const { username, password } = authCredentials;
+
+    const user = new User();
+    user.salt = await bcrypt.genSalt();
+    user.username = username;
+    user.password = await this.hashPassword(password,user.salt);
+
+    try{
+      await user.save();
+    }catch(error){
+      if(error.code == 23505){
+        throw new ConflictException(`Username already exists`);
+      }else{
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateCredentials(authCredentials:AuthCredentialsDto):Promise<string>{
+    const {username, password} = authCredentials;
+    const user = await this.findOne({username});
+    if(user && await user.validatePassword(password)){
+      return user.username
+    }else{
+      return null;
+    }
+  }
+
+  private hashPassword(password:string,salt:string): Promise<string>{
+    return bcrypt.hash(password,salt);
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,43 +1,60 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
-import { CreatTaskDto } from './dto/task.dto';
+import { CreateTaskDto } from './dto/task.dto';
 import { TaskStatusValidation } from './pipes/task-status-validation.pipe';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private taskService:TasksService){
   }
 
   @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto){
-    return this.taskService.getTasks(filterDto);
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTaskFilterDto,
+    @GetUser() user:User
+    ){
+    return this.taskService.getTasks(filterDto,user);
   }
 
   @Get('/:id')
-  getTaskById(@Param('id',ParseIntPipe) id:number): Promise<Task> {
-    return this.taskService.getTaskById(id);
+  getTaskById(
+    @Param('id',ParseIntPipe) id:number,
+    @GetUser() user:User
+    ): Promise<Task> {
+    return this.taskService.getTaskById(id,user);
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() creatTaskDto: CreatTaskDto): Promise<Task>{
-    return this.taskService.createTask(creatTaskDto);
+  createTask(
+    @Body() creatTaskDto: CreateTaskDto,
+    @GetUser() user:User
+    ): Promise<Task>{
+    return this.taskService.createTask(creatTaskDto,user);
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id',ParseIntPipe) id:number): Promise<void> {
-    return this.taskService.deleteTask(id);
+  deleteTask(
+    @Param('id',ParseIntPipe) id:number,
+    @GetUser() user:User
+    ): Promise<void> {
+    return this.taskService.deleteTask(id,user);
   }
 
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id:number,
-    @Body('status',TaskStatusValidation) status: TaskStatus
+    @Body('status',TaskStatusValidation) status: TaskStatus,
+    @GetUser() user:User
   ): Promise<Task> {
-    return this.taskService.updateTaskStatus(id,status);
+    return this.taskService.updateTaskStatus(id,status,user);
   }
 
 
